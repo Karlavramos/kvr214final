6e0efcb68daba4fca2eb103800dec1c1155c5da4
Karlavramos <ramos.karlav@gmail.com>
2026-08-27 11:50:44 -0700

    updated moving average and code

--- scratch/code.R
+++ scratch/code.R
@@ -1,3 +1,5 @@
+# Prepare data -----------------------------------------------------------
+
 library(tidyverse)
 
 PRM_data <- read_csv("data/RioMameyesPuenteRoto.csv")
@@ -5,13 +7,16 @@ bq1_data <- read_csv("data/QuebradaCuenca1-Bisley.csv")
 bq2_data <- read_csv("data/QuebradaCuenca2-Bisley.csv")
 bq3_data <- read_csv("data/QuebradaCuenca3-Bisley.csv")
 
+# source moving average function and apply to each site
 source("R/moving-average.R")
 
-# Initialize Start -------------------------------------------------------
-
-# Need to filter dates first
-
 PRM_moving <- moving_average(PRM_data)
+bq1_moving <- moving_average(bq1_data)
+bq2_moving <- moving_average(bq2_data)
+bq3_moving <- moving_average(bq3_data)
+
+
+# Plot data per site ----------------------------------------------------
 
 PRM_moving |>
   pivot_longer(
@@ -25,4 +30,38 @@ PRM_moving |>
   geom_line() +
   facet_wrap(~Ion)
 
-#done
+bq1_moving |>
+  pivot_longer(
+    cols = c(k_mgl, mg_mgl, no3_ugl, ca_mgl, nh4_ugl),
+    values_to = "Concentration",
+    names_to = "Ion"
+  ) |>
+  ggplot(
+    mapping = aes(x = window_start, y = Concentration, color = Ion)
+  ) +
+  geom_line() +
+  facet_wrap(~Ion)
+
+bq2_moving |>
+  pivot_longer(
+    cols = c(k_mgl, mg_mgl, no3_ugl, ca_mgl, nh4_ugl),
+    values_to = "Concentration",
+    names_to = "Ion"
+  ) |>
+  ggplot(
+    mapping = aes(x = window_start, y = Concentration, color = Ion)
+  ) +
+  geom_line() +
+  facet_wrap(~Ion)
+
+bq3_moving |>
+  pivot_longer(
+    cols = c(k_mgl, mg_mgl, no3_ugl, ca_mgl, nh4_ugl),
+    values_to = "Concentration",
+    names_to = "Ion"
+  ) |>
+  ggplot(
+    mapping = aes(x = window_start, y = Concentration, color = Ion)
+  ) +
+  geom_line() +
+  facet_wrap(~Ion)
