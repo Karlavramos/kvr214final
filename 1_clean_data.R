72cc4647d06a6810b5c376ae370135e1b4e32775
edengonzalezz <edengonzalez@ucsb.edu>
2026-08-27 15:00:25 -0700

    again

--- 1_clean_data.R
+++ 1_clean_data.R
@@ -1,12 +1,23 @@
 library(tidyverse)
+source("R/moving-average.R")
 
 PRM_data <- read_csv("data/RioMameyesPuenteRoto.csv")
 bq1_data <- read_csv("data/QuebradaCuenca1-Bisley.csv")
 bq2_data <- read_csv("data/QuebradaCuenca2-Bisley.csv")
 bq3_data <- read_csv("data/QuebradaCuenca3-Bisley.csv")
 
+
+## sorry i was trying to test something idk if i did anything useful
+test <- PRM_data |> 
+  filter(Sample_Date >= ymd("1988-01-01") & Sample_Date < ymd("1994-12-31") ) |> 
+  select(K, Mg, `NH4-N`, `NO3-N`,Ca)
+
+moving_average(test)
+
+
+
 # source moving average function and apply to each site
-source("R/moving-average.R")
+
 
 PRM_moving <- moving_average(PRM_data)
 bq1_moving <- moving_average(bq1_data)
